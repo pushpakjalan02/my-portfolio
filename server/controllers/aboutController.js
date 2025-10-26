2f1025233f238a6df1a52d7fe1b8dc499f101bfa
Pushpak Jalan <pushpak_jalan@outlook.com>
2025-10-26 09:47:33 +0000

    fix: Default content value for about

--- server/controllers/aboutController.js
+++ server/controllers/aboutController.js
@@ -3,7 +3,7 @@ const About = require('../models/About');
 exports.getAbout = async (req, res) => {
   try {
     const about = await About.findOne();
-    res.json(about || { content: '' });
+    res.json(about || { content: [] });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
